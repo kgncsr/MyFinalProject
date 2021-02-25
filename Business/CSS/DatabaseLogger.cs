e2dea402c8ef9ada3f08a69979f4944f6f1c47c6
kgncsr <69785776+kgncsr@users.noreply.github.com>
2021-02-25 12:50:37 +0300

    Add project files.

---
+++ Business/CSS/DatabaseLogger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.CSS
+{
+    public class DatabaseLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Veritabanı loglandı");
+        }
+    }
+}
---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün eklendi";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz";
+        public static string MaintenanceTime ="Sistem bakımda";
+        public static string ProductsListed ="Ürünler listelendi";
+
+        public static string ProductCountOfCategoryError = "En fazla 10 ürün olabilir ";
+        public static string ProductNameAlreadyExists = "Bu isimde zaten baska ürün var";
+
+        public static string CategoryLimitExceded = "Kategori limiti aşıldığı icin yeni ürün eklenemiyor";
+    }
+}
